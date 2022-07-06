5363688d253f1a7c1f3e358778067a6781274788
Rafael Nobrega <rafael.rlnr@gmail.com>
2022-07-06 14:24:57 -0700

    create loginPageInputs file

---
+++ frontend/src/HomePage/AuthPages/LoginPage/LoginPageInputs.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import TextFieldWithLabel from "../../../shared/components/TextFieldWithLabel";
+
+function LoginPageInputs({ email, setEmail, password, setPassword }) {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <TextFieldWithLabel
+          id="email"
+          label="Email Address"
+          value={email}
+          setValue={setEmail}
+          autoComplete="email"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextFieldWithLabel
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          value={password}
+          setValue={setPassword}
+          autoComplete="current-password"
+        />
+      </Grid>
+    </Grid>
+  );
+}
+
+export default LoginPageInputs;

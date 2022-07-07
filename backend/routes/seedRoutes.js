a59b2f16413663d6034be7cd6d55bfe11a25fab5
Ik Sung Choi <ikchoi0@gmail.com>
2022-07-07 00:18:17 -0700

    .

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { ConsultantSeedDB, CategorySeedDB } = require('../db/seeds');
-const User = require('../models/user');
-const Category = require('../models/category');
+const { ConsultantSeedDB, CategorySeedDB } = require("../db/seeds");
+const User = require("../models/user");
+const Category = require("../models/category");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     await User.deleteMany({});
     const userList = await User.insertMany(ConsultantSeedDB);

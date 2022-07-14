bd28de4e7b637d8d32ef5c0143eb5ea0ff880a2d
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-07-13 22:15:08 -0700

    Booking- cancel -> copy of old booking -> attach to the consultant #85

--- backend/controllers/appointment/updateAppointmentBookedStatus.js
+++ backend/controllers/appointment/updateAppointmentBookedStatus.js
@@ -1,5 +1,5 @@
-const Appointment = require("../../models/appointment");
-const Types = require("mongoose").Types;
+const Appointment = require('../../models/appointment');
+const Types = require('mongoose').Types;
 const updateAppointmentBookedStatus = async (req, res) => {
   try {
     const { appointmentId } = req.body;
@@ -9,14 +9,28 @@ const updateAppointmentBookedStatus = async (req, res) => {
     );
 
     if (!appointment) {
-      return res.status(404).send("Appointment not found");
+      return res.status(404).send('Appointment not found');
     }
 
+    //create a copy of the appointment
+    const appointmentCopy = { ...appointment._doc };
+    console.log('OLD COPY', appointmentCopy);
+
+    // attach the copy appointment to the consultant
+    appointmentCopy.appointmentBooked = false;
+    appointmentCopy.client = null;
+    appointmentCopy.description = null;
+    delete appointmentCopy._id;
+
+    const newCopy = await Appointment.create(appointmentCopy);
+    console.log('NEW COPY', newCopy);
+
     await appointment.updateOne({
-      client: null,
-      appointmentBooked: false,
+      appointmentCancel: true,
+      appointmentCancellation_time: new Date(),
     });
-    console.log("BOOKED CANCELLED", appointment);
+
+    console.log('BOOKED CANCELLED', appointment);
     return res.status(200).send(appointment);
   } catch (error) {
     return res.status(500).send(error.message); // 500 Internal Server Error

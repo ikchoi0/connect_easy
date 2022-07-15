15b609bd2938b5ca974e9f88082634a2a85e0414
Ik Sung Choi <ikchoi0@gmail.com>
2022-07-14 17:48:35 -0700

    add working on chat

---
+++ backend/models/message.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema(
+  {
+    sender: { type: Schema.Types.ObjectId, ref: "User" },
+    message: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Message", messageSchema);

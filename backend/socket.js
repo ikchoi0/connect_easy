f603ae97ffa3bb24b313717127d026a5c4b0c779
Dongwan Kim <dongwan.don.kim@gmail.com>
2022-07-14 21:17:35 -0700

    garbage

--- backend/socket.js
+++ backend/socket.js
@@ -1,29 +1,32 @@
 const socketHandler = (wsServer) => {
   const rooms = {};
-  wsServer.on("connection", (socket) => {
-    socket.on("join_room", (roomName) => {
+  const socketToRoom = {};
+
+  wsServer.on('connection', (socket) => {
+    socket.on('join_room', (roomName) => {
       rooms[socket.id] = roomName;
       socket.join(roomName);
-      socket.to(roomName).emit("welcome");
+      socket.to(roomName).emit('welcome');
     });
 
-    socket.on("offer", (offer, roomName) => {
-      socket.to(roomName).emit("offer", offer);
+    socket.on('offer', (offer, roomName) => {
+      socket.to(roomName).emit('getOffer', offer);
     });
 
-    socket.on("answer", (answer, roomName) => {
-      socket.to(roomName).emit("answer", answer);
+    socket.on('answer', (answer, roomName) => {
+      socket.to(roomName).emit('getAnswer', answer);
     });
 
-    socket.on("ice", (ice, roomName) => {
-      socket.to(roomName).emit("ice", ice);
+    socket.on('ice', (ice, roomName) => {
+      socket.to(roomName).emit('getIce', ice);
     });
 
-    socket.on("disconnect", () => {
+    socket.on('disconnect', () => {
       // console.log(socket.adapter);
+      console.log('disconnected');
       const roomName = rooms[socket.id];
       delete rooms[socket.id];
-      socket.to(roomName).emit("peer_left");
+      socket.to(roomName).emit('peer_left');
     });
   });
 };
